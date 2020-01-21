a5959989157cc0964bcf89974cab80a8045278b3
Szymon Mamczur <szymon.mamczur@vimn.com>
2020-01-21 12:50:24 +0100

    Copy & adjust initial version fo the code

---
+++ Sources/URLSessionDecodable/URLSessionDecodableError.swift
@@ -0,0 +1,24 @@
+//  Copyright © 2020 Viacom. All rights reserved.
+
+import Foundation
+
+public enum URLSessionDecodableError<E: Decodable>: Error {
+    case unknown
+    case deserialization(Deserialization)
+    case nonHTTPResponse(URLResponse)
+    case serverResponse(E)
+    case unknownServerResponse(UnknownServerResponse)
+
+    public struct Deserialization {
+        public let statusCode: Int
+        public let url: URL
+        public let responseBody: Data
+        public let underlyingError: Error?
+    }
+
+    public struct UnknownServerResponse {
+        public let statusCode: Int
+        public let url: URL
+        public let responseBody: Data
+    }
+}
---
+++ Tests/URLSessionDecodableTests/URLSessionDecodableTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import URLSessionDecodable
+
+final class URLSessionDecodableTests: XCTestCase {
+    func testExample() {
+
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
