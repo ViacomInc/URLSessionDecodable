692646b45b7d9bea686abf05a04c2dd495a27ab2
Szymon Mamczur <szymon.mamczur@vimn.com>
2020-01-23 15:31:20 +0100

    Remove StringDecoder

--- Sources/URLSessionDecodable/DataDecoder.swift
+++ Sources/URLSessionDecodable/DataDecoder.swift
@@ -12,26 +12,3 @@ public protocol DataDecoder {
 // MARK: -
 
 extension JSONDecoder: DataDecoder {}
-
-// MARK: -
-
-public struct StringDecoder: DataDecoder {
-
-    public var encoding: String.Encoding = .utf8
-
-    public enum DecodingError: Error {
-        case decoding
-        case notStringType
-    }
-
-    public func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable {
-        guard let string = String(data: data, encoding: encoding) else {
-            throw DecodingError.decoding
-        }
-        guard let castString = string as? T else {
-            throw DecodingError.notStringType
-        }
-        return castString
-    }
-
-}
