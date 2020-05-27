361fde2328a37451ca13a4b559fd1249759dcba0
Kamil Slawinski <kamil.slawinski@vimn.com>
2020-05-27 13:27:16 +0200

    Added public init for URLSessionDecodableError.Deserialization and URLSessionDecodableError.ServerResponse required for NogginPlus unit tests

--- Sources/URLSessionDecodable/URLSessionDecodableError.swift
+++ Sources/URLSessionDecodable/URLSessionDecodableError.swift
@@ -14,12 +14,25 @@ public enum URLSessionDecodableError: Error {
         public let url: URL
         public let responseBody: Data
         public let underlyingError: Error?
+        
+        public init(statusCode: Int, url: URL, responseBody: Data, underlyingError: Error?) {
+            self.statusCode = statusCode
+            self.url = url
+            self.responseBody = responseBody
+            self.underlyingError = underlyingError
+        }
     }
 
     public struct ServerResponse {
         public let statusCode: Int
         public let url: URL
         public let responseBody: Data
+        
+        public init(statusCode: Int, url: URL, responseBody: Data) {
+            self.statusCode = statusCode
+            self.url = url
+            self.responseBody = responseBody
+        }
 
         public func decode<T: Decodable>(using decoder: DataDecoder) throws -> T {
             return try decoder.decode(T.self, from: responseBody)
